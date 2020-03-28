d773188e80ef498b42b322495307b894a9d8fab2
PabloVargasIbarra <pablovargasibarra12@gmail.com>
2020-03-28 18:28:52 +0100

    train model added and _create_pipeline fixed
    train model added and _create_pipeline fixed for unique type columns

--- src/trainer.py
+++ src/trainer.py
@@ -2,14 +2,16 @@
 
 import logging as lg
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
 
-from src.transformers import SelectDtypeColumns, CountThresholder, CategoricalEncoder
-from src.config_loader import ConfigLoader
 from src.logging_config import setup_logging
+from src.config_loader import ConfigLoader
+from src.transformers import SelectDtypeColumns, CountThresholder, CategoricalEncoder
+from src.bayes_hyperopt import BayesOpt
 
 setup_logging()
 CONFIG = ConfigLoader()
@@ -22,11 +24,12 @@ class Trainer:
 
         self._config = config
         self.data = None
-        # self.model_class = None
+        self.categorical_columns = None
+        self.numerical_columns = None
         self.model = None
         self.parameters_space = None
-        self.train = None
-        self.val = None
+        self.df_train = None
+        self.df_test = None
         self.metadata = None
 
     @property
@@ -36,13 +39,17 @@ class Trainer:
     def run(self):
         self.load_data()
         self.preprocessing()
+        self.qa_data()
         self.split_data()
+        self.train_model()
 
     def load_data(self):
         # TODO: Implement loading process from S3
         logger = lg.getLogger(self.load_data.__name__)
         logger.info('Loading data')
+        # https://www.kaggle.com/uciml/breast-cancer-wisconsin-data
         self.data = pd.read_csv(self._config['local_data_path'])
+        self.data.diagnosis.replace({'M': 1, 'B': 0}, inplace=True)
         logger.info('Data columns: %s', self.data.columns)
         logger.info('Data head: \n %s', self.data.head())
 
@@ -54,34 +61,51 @@ class Trainer:
         logger = lg.getLogger(self.preprocessing.__name__)
         logger.info('Preprocessing data')
         self.data.dropna(axis='columns', how='all', inplace=True)
+        self.data['cat'] = np.random.choice(['a', 'b'], size=self.data.shape[0])
         self.data.select_dtypes(exclude=['number', 'bool']).fillna('nan')
         logger.info('Data head: \n %s', self.data.head())
         logger.info(self.data.columns)
 
-    # def qa_data(self):
-    # TODO: Add quality data analysis
-    #     pass
+    def qa_data(self):
+        logger = lg.getLogger(self.qa_data.__name__)
+        # TODO: Add quality data analysis
+        self.categorical_columns = set(self.data.select_dtypes(exclude=['number', 'bool']).columns)
+        logger.info('Categorical columns: [%s]', self.categorical_columns)
+        self.numerical_columns = set(self.data.select_dtypes(include=['number', 'bool']).columns)
+        logger.info('Numerical columns: [%s]', self.numerical_columns)
 
     def split_data(self):
         logger = lg.getLogger(self.split_data.__name__)
-        self.train, self.val = train_test_split(self.data, train_size=self._config['train_size'])
-        logger.info('Training dataset shape: %s head: \n %s', self.train.shape,  self.train.head())
-        logger.info('Validation dataset shape: %s head: \n %s', self.val.shape,  self.val.head())
+        self.df_train, self.df_test = train_test_split(self.data, train_size=self._config['train_size'])
+        logger.info('Training dataset shape: %s head: \n %s', self.df_train.shape, self.df_train.head())
+        logger.info('Test dataset shape: %s head: \n %s', self.df_test.shape, self.df_test.head())
 
-
-    @staticmethod
-    def _create_pipeline(search_space):
+    def _create_pipeline(self, search_space):
         # TODO: Parametrize the model defined
-        pipeline = make_pipeline(make_union(make_pipeline(SelectDtypeColumns(exclude=['number', 'bool']),
-                                                          CountThresholder(**search_space['countthresholder']),
-                                                          CategoricalEncoder(**search_space['categoricalencoder'])),
-                                            make_pipeline(SelectDtypeColumns(include=['number', 'bool']),
-                                                          SimpleImputer(**search_space['simpleimputer']))),
-                                 LogisticRegression(solver='liblinear', **search_space['logisticregression']))
+        numerical_encoder = make_pipeline(SelectDtypeColumns(include=['number', 'bool']),
+                                          SimpleImputer(**search_space['simpleimputer']))
+        categorical_encoder = make_pipeline(SelectDtypeColumns(exclude=['number', 'bool']),
+                                            CountThresholder(**search_space['countthresholder']),
+                                            CategoricalEncoder(**search_space['categoricalencoder']))
+        if self.categorical_columns and self.numerical_columns:
+            encoder = make_union(numerical_encoder, categorical_encoder)
+        elif self.numerical_columns:
+            encoder = numerical_encoder
+        else:
+            encoder = categorical_encoder
+
+        model_object = LogisticRegression(solver='liblinear', **search_space['logisticregression'])
+        pipeline = make_pipeline(encoder, model_object)
         return pipeline
 
     def train_model(self):
-        pass
+        logger = lg.getLogger(self.train_model.__name__)
+        bayes = BayesOpt(self._config)
+        x = self.df_train.drop(columns=self.target_column)
+        y = self.df_train[self.target_column].values
+        logger.info('X dataset shape: %s head: \n %s', x.shape, x)
+        best_params = bayes.optimization(self._create_pipeline, x, y)
+        logger.info(best_params)
 
     def evaluate_model(self):
         pass
