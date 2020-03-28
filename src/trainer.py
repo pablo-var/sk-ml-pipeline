40d6d560236a47ec18c6b39945ba03e217b07ac2
PabloVargasIbarra <pablovargasibarra12@gmail.com>
2020-03-28 11:36:20 +0100

    Implement initial data loading and preprocessing
    Implement initial data loading and preprocessing

--- src/trainer.py
+++ src/trainer.py
@@ -1,41 +1,73 @@
 """Trainer module"""
 
+import logging as lg
+import pandas as pd
+
+from src.config_loader import ConfigLoader
+from src.logging_config import setup_logging
+
+setup_logging()
+CONFIG = ConfigLoader()
+CONFIG.load('config/config.yaml')
+
 
 class Trainer:
 
-    def __init__(self):
+    def __init__(self, config):
 
-        self._config = None
-        self.model_class = None
+        self._config = config
+        self.data = None
+        # self.model_class = None
         self.model = None
         self.parameters_space = None
-        self.target_name = None
-        self.data = None
         self.train = None
         self.valid = None
         self.metadata = None
 
+    @property
+    def target_column(self):
+        return self._config['target_column']
+
     def run(self):
-        pass
+        self.load_data()
+        self.preprocessing()
 
-    def _load_data(self):
-        pass
+    def load_data(self):
+        # TODO: Implement loading process from S3
+        logger = lg.getLogger(self.load_data.__name__)
+        logger.info('Loading data')
+        self.data = pd.read_csv(self._config['local_data_path'])
+        logger.info(self.data.head())
+        logger.info(self.data.columns)
 
-    def _basic_preprocessing(self):
-        """Fillna NaN in categorical features as 'nan'
+    def preprocessing(self):
         """
+        - Remove full NaN columns
+        - Fillna NaN in categorical features as 'nan'
+        """
+        logger = lg.getLogger(self.preprocessing.__name__)
+        logger.info('Preprocessing data')
+        self.data.dropna(axis='columns', how='all', inplace=True)
+        self.data.select_dtypes(exclude=['number', 'bool']).fillna('nan')
+        logger.info(self.data.head())
+        logger.info(self.data.columns)
 
-    def _qa_data(self):
+    def qa_data(self):
         pass
 
-    def _split_data(self):
+    def split_data(self):
         pass
 
-    def _train_model(self):
+    def train_model(self):
         pass
 
-    def _evaluate_model(self):
+    def evaluate_model(self):
         pass
 
-    def _save_artifacts(self):
+    def save_artifacts(self):
         pass
+
+
+if __name__ == '__main__':
+    trainer = Trainer(CONFIG)
+    trainer.run()
